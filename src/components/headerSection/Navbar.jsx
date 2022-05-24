1381b6c86c4ab62104c416894f4af9c5e0cd52d0
zeus012345 <francisjerry360@gmail.com>
2022-05-24 02:12:09 +0100

    master

--- src/components/headerSection/Navbar.jsx
+++ src/components/headerSection/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                 <i className={open ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'}></i>
             </div>
 
-            <ul className={`lg:flex items-center gap-4 pt-5 pb-16 lg:py-2 nav-ink text-center transition-all lg:transition-none z-[-50] duration-500 absolute lg:static ease-in bg-gradient-to-r from-[#C9DEFD] via-[#FFFFFE] to-[#F8ECCA] lg:bg-white w-full lg:w-auto left-0 ${open ? 'top-20' : 'top-[-490px]'}`}>
+            <ul className={`lg:flex items-center gap-4 pt-5 pb-16 lg:py-2 nav-ink text-center transition-all lg:transition-none z-[-50] duration-500 absolute lg:static ease-in bg-gradient-to-r from-[#C9DEFD] via-[#FFFFFE] to-[#F8ECCA] lg:bg-white w-full lg:w-auto left-0 ${open ? 'top-20' : 'left-[-490px]'}`}>
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="./Market.jsx">Market</NavLink></li>
                 <li><NavLink to="./News.jsx">News</NavLink></li>
